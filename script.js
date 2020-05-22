69ee88ce50caf84f52397145824aaf8ab9916391
katelyn18 <34407045+katelyn18@users.noreply.github.com>
2020-05-21 20:28:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+var questions_array = [
+    {question: "What color are strawberries?",
+    option1: "(a) red",
+    option2: "(b) blue",
+    answer: "option1"},
+    {question: "What color are bananas?",
+    option1: "(a) purple",
+    option2: "(b) yellow",
+    answer: "option2"},
+    {question: "What color are apples?",
+    option1: "(a) red/green",
+    option2: "(b) orange",
+    answer: "option1"}
+];
+var score; //number of correct answers
+var button_clicked; //answer chosen
+var next_question; //go to next question
+
+//show the first question
+function startGame(){
+    score = 0;
+    next_question = 0;
+    document.getElementById("questions_placeholder").innerHTML = questions_array[ next_question ].question;
+    document.getElementById("option1").value = questions_array[ next_question ].option1;
+    document.getElementById("option2").value = questions_array[ next_question ].option2;
+    document.getElementById("user_score").style.display="none";
+    document.getElementById("restart").style.display = "none";
+}
+
+//determine if answer clicked is correct or incorrect
+function isCorrect(btn){
+    button_clicked = btn.id;
+    if( questions_array[ next_question ].answer == btn.id ){
+        //alert("correct!");
+        document.getElementById(btn.id).style.backgroundColor = "green";
+        document.getElementById(btn.id).style.color = "white";
+        score++;
+    }
+    else{
+        //alert("WRONG");
+        document.getElementById(btn.id).style.backgroundColor = "red";
+        document.getElementById(btn.id).style.color = "white";
+    }
+}
+
+//get the next question or end the game
+function nextQuestion(btn){
+    next_question++;
+    //check to make sure an answer is chosen before going to the next question
+    if(button_clicked == null){ 
+        alert("Please choose an answer");
+        next_question--;
+    }
+    else {
+        if( next_question < questions_array.length ){
+            document.getElementById(button_clicked).style.backgroundColor = "white";
+            document.getElementById(button_clicked).style.color = "black";
+            document.getElementById("questions_placeholder").innerHTML = questions_array[ next_question ].question;
+            document.getElementById("option1").value = questions_array[ next_question ].option1;
+            document.getElementById("option2").value = questions_array[ next_question ].option2;
+        }
+        else{
+            document.getElementById("questions_placeholder").innerHTML = "Thank you for playing :)";
+            document.getElementById("option1").style.display = "none";
+            document.getElementById("option2").style.display = "none";  
+            document.getElementById("user_score").style.display = "initial";
+            document.getElementById("user_score").innerHTML = "Your score: " + score + "/" + questions_array.length;
+            document.getElementById("next").style.display = "none";
+            document.getElementById("restart").style.display = "initial";
+        }
+        button_clicked = null;
+    }
+}
+
+function restart(){
+    document.getElementById("option1").style.display = "initial";
+    document.getElementById("option1").style.backgroundColor = "white";
+    document.getElementById("option1").style.color = "black";
+
+    document.getElementById("option2").style.display = "initial"; 
+    document.getElementById("option2").style.backgroundColor = "white";  
+    document.getElementById("option2").style.color = "black";   
+    
+    document.getElementById("user_score").style.display = "none";
+    document.getElementById("next").style.display = "initial";
+    document.getElementById("restart").style.display = "none";
+    //alert("RESTART");
+    startGame();
+}
